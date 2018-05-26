1a379ecc1f7dcd6f55e05ec12e4ee92bbf2d6e28
Jaime Bellmyer <online@bellmyer.com>
2018-05-25 21:45:54 -0500

    validate numericality of numeric fellow fields

--- app/models/fellow.rb
+++ app/models/fellow.rb
@@ -10,6 +10,13 @@ class Fellow < ApplicationRecord
   
   validates :first_name, :last_name, :employment_status_id, presence: true
   
+  validates :graduation_semester, inclusion: {in: Course::VALID_SEMESTERS}
+  validates :graduation_year, numericality: {greater_than: 2010, less_than: 2050, allow_nil: true, only_integer: true}
+  validates :graduation_fiscal_year, numericality: {greater_than: 2010, less_than: 2050, allow_nil: true, only_integer: true}
+  
+  validates :gpa, numericality: {greater_than_or_equal_to: 0.0, less_than_or_equal_to: 4.0, allow_nil: true}
+  validates :efficacy_score, numericality: {greater_than_or_equal_to: 0.0, less_than_or_equal_to: 1.0, allow_nil: true}
+  
   def cohort
     cohorts.order('id desc').first
   end
--- spec/models/fellow_spec.rb
+++ spec/models/fellow_spec.rb
@@ -28,6 +28,30 @@ RSpec.describe Fellow, type: :model do
     it { should validate_presence_of(attribute) }
   end
   
+  it { should validate_inclusion_of(:graduation_semester).in_array(Course::VALID_SEMESTERS) }
+  
+  it { should validate_numericality_of(:graduation_year).is_greater_than(2010) }
+  it { should validate_numericality_of(:graduation_year).is_less_than(2050) }
+  it { should validate_numericality_of(:graduation_year).only_integer }
+  it { should validate_numericality_of(:graduation_year).allow_nil }
+  
+  it { should validate_numericality_of(:graduation_fiscal_year).is_greater_than(2010) }
+  it { should validate_numericality_of(:graduation_fiscal_year).is_less_than(2050) }
+  it { should validate_numericality_of(:graduation_fiscal_year).only_integer }
+  it { should validate_numericality_of(:graduation_fiscal_year).allow_nil }
+  
+  it { should validate_numericality_of(:gpa).is_greater_than_or_equal_to(0.0) }
+  it { should validate_numericality_of(:gpa).is_less_than_or_equal_to(4.0) }
+  it { should validate_numericality_of(:gpa).allow_nil }
+  
+  it { should validate_numericality_of(:efficacy_score).is_greater_than_or_equal_to(0.0) }
+  it { should validate_numericality_of(:efficacy_score).is_less_than_or_equal_to(1.0) }
+  it { should validate_numericality_of(:efficacy_score).allow_nil }
+  
+  ##################
+  # Instance methods
+  ##################
+  
   describe "#cohort" do
     def add_cohort
       create(:cohort_fellow, fellow: fellow).cohort
