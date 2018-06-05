d580e4713903ea4524831aefc406f1b68c0248f2
Jaime Bellmyer <online@bellmyer.com>
2018-06-05 15:32:18 -0500

    add redirect to new opp after adding employer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :set_requested_redirect
+  
+  private
+  
+  def set_requested_redirect
+    if params[:redirect]
+      session[:requested_redirect] = params[:redirect]
+    end
+  end
+  
+  def follow_redirect
+    requested_redirect = session.delete(:requested_redirect)
+    
+    case requested_redirect
+    when 'new_opportunity'
+      new_employer_opportunity_path(@employer)
+    else
+      nil
+    end
+  end
 end
