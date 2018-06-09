b4053cb4758675f59c953a17da15f6c7c807e56f
Jaime Bellmyer <online@bellmyer.com>
2018-06-09 16:23:38 -0500

    contact object inline with location form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,8 @@
 class Location < ApplicationRecord
   belongs_to :locateable, polymorphic: true
+
   has_one :contact, as: :contactable
+  accepts_nested_attributes_for :contact
   
   has_and_belongs_to_many :opportunities
   
