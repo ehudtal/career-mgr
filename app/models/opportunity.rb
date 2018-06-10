5783cb1f69ac4c170f99f649859b030c2843d78a
Jaime Bellmyer <online@bellmyer.com>
2018-06-10 10:31:48 -0500

    add nested attributes for opp locations

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -2,12 +2,14 @@ class Opportunity < ApplicationRecord
   belongs_to :employer
   
   has_many :tasks, as: :taskable
+  accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
   
   has_and_belongs_to_many :industries
   has_and_belongs_to_many :interests
+
   has_and_belongs_to_many :locations
+  accepts_nested_attributes_for :locations, reject_if: :all_blank, allow_destroy: true
   
-  accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 
   validates :name, presence: true
   validates :job_posting_url, url: {ensure_protocol: true}, allow_blank: true
