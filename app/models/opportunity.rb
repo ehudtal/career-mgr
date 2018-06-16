aaffcfde857e0dfe2d527c43dbbe840071d081d9
Jaime Bellmyer <online@bellmyer.com>
2018-06-16 16:12:35 -0500

    cleanup

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -13,7 +13,6 @@ class Opportunity < ApplicationRecord
   has_and_belongs_to_many :locations, dependent: :destroy
   accepts_nested_attributes_for :locations, reject_if: :all_blank, allow_destroy: true
   
-
   validates :name, presence: true
   validates :job_posting_url, url: {ensure_protocol: true}, allow_blank: true
   
