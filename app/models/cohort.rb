f16134bfc80ab848d57ff056a7d8d36e68d235dc
Jaime Bellmyer <online@bellmyer.com>
2018-06-26 07:16:08 -0500

    refactor cohort.create_or_update into fellows association

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -4,25 +4,40 @@ class Cohort < ApplicationRecord
   belongs_to :course
   
   has_many :cohort_fellows
-  has_many :fellows, through: :cohort_fellows
+
+  has_many :fellows, through: :cohort_fellows do
+    def create_or_update attributes
+      contact_attributes = attributes.slice(*Contact.attribute_names.map(&:to_sym))
+      fellow_attributes = attributes.slice(*(attributes.keys - contact_attributes.keys))
+      
+      if fellow = unique(attributes)
+        fellow.update fellow_attributes
+        fellow.contact.update contact_attributes
+      else
+        fellow = create fellow_attributes.merge({
+          contact_attributes: contact_attributes
+        })
+      end
+      
+      fellow
+    end
+    
+    def unique attributes
+      detect do |fellow|
+        fellow_attrs = {first_name: fellow.first_name, last_name: fellow.last_name, phone: fellow.contact.phone, email: fellow.contact.email}
+      
+        hashes_equal(attributes, fellow_attrs, [:phone, :email]) ||
+        hashes_equal(attributes, fellow_attrs, [:phone, :first_name, :last_name]) ||
+        hashes_equal(attributes, fellow_attrs, [:email, :first_name, :last_name])
+      end
+    end
+
+    def hashes_equal first, second, attributes
+      first.slice(*attributes) == second.slice(*attributes)
+    end
+  end
   
   has_and_belongs_to_many :coaches
   
   validates :name, presence: true, uniqueness: {scope: :course_id}
-
-  def unique_fellow attributes
-    fellows.detect do |fellow|
-      fellow_attrs = {first_name: fellow.first_name, last_name: fellow.last_name, phone: fellow.contact.phone, email: fellow.contact.email}
-      
-      hashes_equal(attributes, fellow_attrs, [:phone, :email]) ||
-      hashes_equal(attributes, fellow_attrs, [:phone, :first_name, :last_name]) ||
-      hashes_equal(attributes, fellow_attrs, [:email, :first_name, :last_name])
-    end
-  end
-
-  private
-  
-  def hashes_equal first, second, attributes
-    first.slice(*attributes) == second.slice(*attributes)
-  end
 end
--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -27,51 +27,79 @@ RSpec.describe Cohort, type: :model do
   # Instance methods
   ##################
   
-  describe '#unique_fellow(attributes)' do
-    let(:cohort) { build :cohort}
-    let(:fellow) { build :fellow }
-    let(:contact) { build :contact }
+  describe '#fellows.unique(attributes)' do
+    let(:cohort) { create :cohort}
+    let(:fellow) { create :fellow, contact: contact }
+    let(:contact) { create :contact }
     let(:attributes) { {first_name: fellow.first_name, last_name: fellow.last_name, email: contact.email, phone: contact.phone} }
     
     before do
-      allow(cohort).to receive(:fellows).and_return([fellow])
-      allow(fellow).to receive(:contact).and_return(contact)
+      cohort.fellows << fellow
     end
     
     it "recognizes an exact name/phone/email match" do
-      expect(cohort.unique_fellow(attributes)).to eq(fellow)
+      expect(cohort.fellows.unique(attributes)).to eq(fellow)
     end
     
     it "accepts extra parameters" do
       attributes[:extra] = 'extra'
-      expect(cohort.unique_fellow(attributes)).to eq(fellow)
+      expect(cohort.fellows.unique(attributes)).to eq(fellow)
     end
     
     it "recognizes fellow when phone has changed" do
       attributes[:phone] = '111-222-3333'
-      expect(cohort.unique_fellow(attributes)).to eq(fellow)
+      expect(cohort.fellows.unique(attributes)).to eq(fellow)
     end
     
     it "recognizes fellow when email has changed" do
       attributes[:email] = 'nothing@example.com'
-      expect(cohort.unique_fellow(attributes)).to eq(fellow)
+      expect(cohort.fellows.unique(attributes)).to eq(fellow)
     end
     
     it "recognizes fellow when first name has changed" do
       attributes[:first_name] = 'nothing'
-      expect(cohort.unique_fellow(attributes)).to eq(fellow)
+      expect(cohort.fellows.unique(attributes)).to eq(fellow)
     end
 
     it "recognizes fellow when last name has chaned" do
       attributes[:last_name] = 'nothing'
-      expect(cohort.unique_fellow(attributes)).to eq(fellow)
+      expect(cohort.fellows.unique(attributes)).to eq(fellow)
     end
     
     it "does not recognize fellow when phone AND e-mail are different" do
       attributes[:phone] = '111-222-3333'
       attributes[:email] = 'nothing@example.com'
       
-      expect(cohort.unique_fellow(attributes)).to be_nil
+      expect(cohort.fellows.unique(attributes)).to be_nil
+    end
+  end
+  
+  describe '#fellows.create_or_update(attributes)' do
+    it "updates when it recognizes the fellow" do
+      contact = create :contact
+      fellow = create :fellow, contact: contact
+      cohort = create :cohort
+      attributes = {first_name: fellow.first_name, last_name: fellow.last_name, phone: contact.phone, email: contact.email}
+      
+      allow(cohort.fellows).to receive(:unique).with(attributes).and_return(fellow)
+      
+      attributes[:first_name] = 'NewFirstName'
+      cohort.fellows.create_or_update(attributes)
+      
+      expect(fellow.reload.first_name).to eq('NewFirstName')
+    end
+    
+    it "creates when it doesn't recognize the fellow" do
+      cohort = create :cohort
+      attributes = {first_name: 'Bob', last_name: 'Smith', email: 'bob@example.com', phone: '111-222-3333'}
+      
+      cohort.fellows.create_or_update(attributes)
+      fellow = cohort.fellows.first
+      
+      expect(fellow.first_name).to eq(attributes[:first_name])
+      expect(fellow.last_name).to eq(attributes[:last_name])
+      expect(fellow.contact.email).to eq(attributes[:email])
+      expect(fellow.contact.phone).to eq(attributes[:phone])
     end
   end
 end
