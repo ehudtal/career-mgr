3e63e43c71f1fab097523fe27ad74c3caf37f4ec
Jaime Bellmyer <online@bellmyer.com>
2018-07-18 17:07:22 -0500

    add admin and fellow roles to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_221727) do
+ActiveRecord::Schema.define(version: 2018_07_18_222830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -326,8 +326,12 @@ ActiveRecord::Schema.define(version: 2018_07_10_221727) do
     t.boolean "is_administrator"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.boolean "fellow", default: true
+    t.index ["admin"], name: "index_users_on_admin"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["fellow"], name: "index_users_on_fellow"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :user do
     sequence(:email){|i| "user#{i}@example.com"}
     password 'password'
+    
+    factory :admin_user do
+      admin true
+      fellow false
+    end
+    
+    factory :fellow_user do
+      admin false
+      fellow true
+    end
   end
 end
