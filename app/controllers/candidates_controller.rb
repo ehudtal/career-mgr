ffd3fbbda3c93c49b475a0c54be8302177a5069e
Jaime Bellmyer <online@bellmyer.com>
2018-07-24 17:43:40 -0500

    cleanup typos

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,8 +2,8 @@ class CandidatesController < ApplicationController
   before_action :authorize_by_token!
   
   def status
-    if fellow_opportunity = FellowOpportunity.find_by(id: params[:fellow_opportunity_id])
-      fellow_opportunity.stage = params[:update]
+    if @fellow_opportunity = FellowOpportunity.find_by(id: params[:fellow_opportunity_id])
+      @fellow_opportunity.stage = params[:update].downcase
     else
       fail_token_authorize!
     end
