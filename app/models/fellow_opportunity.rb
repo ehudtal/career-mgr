07a8fdd5d4e0d488837c76b2af12813f3d2581d1
Jaime Bellmyer <online@bellmyer.com>
2018-07-24 09:26:57 -0500

    create shortcut accessors for fellow_opp opportunity_stages

--- app/models/fellow_opportunity.rb
+++ app/models/fellow_opportunity.rb
@@ -10,4 +10,12 @@ class FellowOpportunity < ApplicationRecord
   validates :fellow_id, presence: true, uniqueness: {scope: :opportunity_id}
   validates :opportunity_id, presence: true
   validates :opportunity_stage_id, presence: true
+  
+  def stage
+    opportunity_stage.name
+  end
+  
+  def stage= stage_name
+    self.update opportunity_stage: OpportunityStage.find_by(name: stage_name)
+  end
 end
--- spec/models/fellow_opportunity_spec.rb
+++ spec/models/fellow_opportunity_spec.rb
@@ -24,4 +24,26 @@ RSpec.describe FellowOpportunity, type: :model do
     subject { create :fellow_opportunity }
     it { should validate_uniqueness_of(:fellow_id).scoped_to(:opportunity_id) }
   end
+  
+  ##################
+  # Instance methods
+  ##################
+
+  describe '#stage accessors' do
+    let(:stage_name) { 'stage name' }
+    let(:opportunity_stage) { create :opportunity_stage, name: stage_name }
+    let(:fellow_opportunity) { create :fellow_opportunity }
+    
+    before { fellow_opportunity; opportunity_stage }
+
+    it "returns the name of the opportunity stage" do
+      fellow_opportunity.opportunity_stage = opportunity_stage
+      expect(fellow_opportunity.stage).to eq(stage_name)
+    end
+
+    it "sets the opportunity_stage based on name" do
+      fellow_opportunity.stage = stage_name
+      expect(fellow_opportunity.reload.opportunity_stage).to eq(opportunity_stage)
+    end
+  end
 end
