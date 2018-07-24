e1337c86996a303ddf9a89d4927a61e6b757055e
Jaime Bellmyer <online@bellmyer.com>
2018-07-24 12:34:02 -0500

    create link_to helper for access tokens

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require 'uri'
+
 module ApplicationHelper
+  def link_to_access_token access_token, label
+    route = access_token.route_for(label)
+    
+    uri = URI.parse(route['path'])
+    new_query = URI.decode_www_form(uri.query || '') << ['token', access_token.code]
+    uri.query = URI.encode_www_form(new_query)
+    
+    link_to route['label'], uri.to_s, method: route['method']
+  end
 end
