1f5a04a87602bd07dbc56275e570e576e0c5d309
Jaime Bellmyer <online@bellmyer.com>
2018-07-27 14:34:41 -0500

    reset opportunity stages

--- db/init.rb
+++ db/init.rb
@@ -46,18 +46,21 @@ OpportunityStage.create!([
   {position: 2,  probability: 0.0,  togglable: false, name: 'not interested'},
   {position: 3,  probability: 0.1,  togglable: true,  name: 'researched employer'},
 
-  {position: 4,  probability: 0.1,  togglable: true,  name: 'connected with employees'},
-  {position: 5,  probability: 0.1,  togglable: true,  name: 'customized application materials'},
-  {position: 6,  probability: 0.1,  togglable: true,  name: 'submitted application'},
-  {position: 7,  probability: 0.1,  togglable: true,  name: 'followed up after application submission'},
+  {position: 4,  probability: 0.15, togglable: true,  name: 'connected with employees'},
+  {position: 5,  probability: 0.2,  togglable: true,  name: 'customized application materials'},
+  {position: 6,  probability: 0.25, togglable: true,  name: 'submitted application'},
+  {position: 7,  probability: 0.3,  togglable: true,  name: 'followed up after application submission'},
 
-  {position: 8,  probability: 0.1,  togglable: true,  name: 'scheduled an interview'},
-  {position: 9,  probability: 0.1,  togglable: true,  name: 'researched interview process'},
-  {position: 10, probability: 0.1,  togglable: true,  name: 'practiced for interview'},
-  {position: 11, probability: 0.1,  togglable: true,  name: 'attended interview'},
-  {position: 12, probability: 0.1,  togglable: true,  name: 'followed up after interview'},
+  {position: 8,  probability: 0.35, togglable: true,  name: 'scheduled an interview'},
+  {position: 9,  probability: 0.4,  togglable: true,  name: 'researched interview process'},
+  {position: 10, probability: 0.45, togglable: true,  name: 'practiced for interview'},
+  {position: 11, probability: 0.5,  togglable: true,  name: 'attended interview'},
+  {position: 12, probability: 0.6,  togglable: true,  name: 'followed up after interview'},
 
-  {position: 13, probability: 0.0,  togglable: false, name: 'rejected'}
+  {position: 13, probability: 0.9,  togglable: true,  name: 'received offer'},
+  {position: 14, probability: 0.95, togglable: true,  name: 'submitted counter-offer'},
+  {position: 15, probability: 1.0,  togglable: true,  name: 'accepted offer'},
+  {position: 16, probability: 0.0,  togglable: false, name: 'rejected'}
 ])
 
 nlu = Site.find_or_create_by code: 'NLU', name: 'National Louis University'
