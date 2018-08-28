e085cc1bb4622773d366961e10c097362c77b1e2
Jaime Bellmyer <online@bellmyer.com>
2018-08-28 11:33:23 -0500

    add basic flash notices to opportunity stage updates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,7 +3,12 @@ class CandidatesController < ApplicationController
   
   def status
     if @fellow_opportunity = FellowOpportunity.find_by(id: params[:fellow_opportunity_id])
-      @fellow_opportunity.update_stage(params[:update].downcase, from: params[:from])
+      update = params[:update].downcase
+      notice = @fellow_opportunity.opportunity_stage.content['notices'][update]
+      
+      @fellow_opportunity.update_stage(update, from: params[:from])
+      
+      flash[:stage_notice] = notice if notice
       redirect_to fellow_opportunity_path(@fellow_opportunity)
     else
       fail_token_authorize!
