5c05a8cf294309d85723ef8969a3f29ed04b257a
Jaime Bellmyer <online@bellmyer.com>
2018-08-31 14:59:05 -0500

    secretify/clean nlu sso url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,21 +39,15 @@ module ApplicationHelper
     distance ? pluralize(distance.round, 'mile') : 'N/A'
   end
   
-  def sso_encode repeat=1
-    url = "#{request.base_url}/users/service"
-    
-    repeat.times do
-      url = URI.encode(url, /[^\-_!~*'()a-zA-Z\d;?@&=+$,\[\]]/)
-    end
-    
-    url
+  def sso_encode
+    URI.encode("#{request.base_url}/users/service", /[^\-_!~*'()a-zA-Z\d;?@&=+$,\[\]]/)
   end
   
   def braven_login
-    "#{Rails.application.secrets.sso_url}login?service=#{sso_encode()}"
+    "#{Rails.application.secrets.sso_url}login?service=#{sso_encode}"
   end
   
   def nlu_login
-    "https://sts.nl.edu/adfs/ls/?wa=wsignin1.0&wtrealm=https%3a%2f%2fcps66.quicklaunchsso.com%2f&wctx=rm%3d0%26id%3dpassive%26ru%3d%252fcas%252flogin%253fservice%253d#{sso_encode(3)}&wct=2018-08-30T22%3a01%3a32Z"
+    "#{Rails.application.secrets.nlu_sso_url}login?service=#{sso_encode}"
   end
 end
