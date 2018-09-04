1e7536fb97ed973166b8adc716cc070958a757fb
Jaime Bellmyer <online@bellmyer.com>
2018-09-04 14:04:27 -0500

    add routes for unsubscribe

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -39,7 +39,8 @@ class AccessToken < ApplicationRecord
       [
         {label: 'view', method: 'GET', params: {controller: 'admin/fellows', action: 'show', id: fellow.id.to_s}},
         {label: 'Edit Your Profile', method: 'GET', params: {controller: 'fellows', action: 'edit', id: fellow.id.to_s}},
-        {label: 'Update Your Profile', method: 'PUT', params: {controller: 'fellows', action: 'update', id: fellow.id.to_s}}
+        {label: 'Update Your Profile', method: 'PUT', params: {controller: 'fellows', action: 'update', id: fellow.id.to_s}},
+        {label: 'Unsubscribe', method: 'GET', params: {controller: 'fellow/profile', action: 'unsubscribe'}}
       ]
     end
     
