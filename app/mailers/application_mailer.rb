e73b1e2065d5773048185d3642cd64908dd1f6a8
Jaime Bellmyer <online@bellmyer.com>
2018-09-04 13:03:11 -0500

    disable default mail method to ensure subscriber-safe method is always used

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,11 +2,15 @@ class ApplicationMailer < ActionMailer::Base
   default from: Rails.application.secrets.mailer_from_email
   layout 'mailer'
   
-  private
+  alias_method :direct_mail, :mail
+  
+  def mail options={}
+    raise "Do not use the mail method directly, use mail_subscribed to prevent mailing unsubscribers."
+  end
   
   def mail_subscribed subscribed, params={}
     if subscribed
-      mail(params)
+      direct_mail(params)
     else
       Rails.logger.info("COULD NOT E-MAIL #{params[:to]} due to subscription preferences")
     end
