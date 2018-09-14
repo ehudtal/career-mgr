ceb0cadf1e21b1576716e90fe761c30164f79834
Admin Server <admin@bebraven.org>
2018-09-14 13:28:49 +0000

    Add some logging to the auto-nag cronjob script

--- scripts/run_auto_nag.sh
+++ scripts/run_auto_nag.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ..
+echo "`date`: Running RAILS_ENV=production bundle exec rake candidate_mailer:send_notifications from: `pwd`"
 RAILS_ENV=production bundle exec rake candidate_mailer:send_notifications
